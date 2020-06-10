fb81d83a73a365d8b504f2d87707f41f3e7c871f
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2020-06-10 00:40:57 -0400

    More code

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,13 @@
 //! in Rust through the use of a procedural macros.
 #![recursion_limit = "128"]
 
-use syn::{Expr, Ident, Member};
-use syn::parse::ParseStream;
-use proc_macro::TokenStream;
+use syn::{Field, Ident, Result, Token, Expr, Member};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
 
+use quote::quote;
+
+use proc_macro::TokenStream;
 use std::collections::HashMap;
 
 
@@ -17,11 +20,46 @@ trait ToHashMap<T> {
     fn to_hashmap(structure: ParseStream) -> HashMap<String, String>;
 }
 
+struct ItemStruct {
+    struct_token: Token![struct],
+    ident: Ident,
+    brace_token: syn::token::Brace,
+    fields: Punctuated<Field, Token![,]>,
+}
+
+impl Parse for ItemStruct {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(ItemStruct {
+            struct_token: input.parse()?,
+            ident: input.parse()?,
+            brace_token: syn::braced!(content in input),
+            fields: content.parse_terminated(Field::parse_named)?,
+        })
+    }
+}
 
 #[proc_macro_derive(FromHashMap)]
 pub fn from_hashmap(input: TokenStream) -> TokenStream {
+	let ast = syn::parse_macro_input!(input as ItemStruct);
 
-    // turn source into a parsable string for AST conversion
+	// parse out all the field names in the struct
+	let idents: Vec<&Ident> = ast.fields
+		.iter()
+		.filter_map(|field| field.ident.as_ref())
+		.collect::<Vec<&Ident>>();
+
+	// get the name identifier of the struct input AST
+	let name: &Ident = &ast.ident;
+
+	// start codegen of the impl for the given struct
+	todo!()
+}
+
+
+#[proc_macro_derive(ToHashMap)]
+pub fn to_hashmap(input: TokenStream) -> TokenStream {
+	// turn source into a parsable string for AST conversion
     let source: String = input.to_string();
     let ast: Expr = syn::parse_str::<Expr>(&source).unwrap();
 
@@ -43,17 +81,5 @@ pub fn from_hashmap(input: TokenStream) -> TokenStream {
             panic!("does not support non-product abstract data types");
         }
     };
-
-
-    // dynamic code generation time, given the set of fields parsed out
-    let struct_name
-
-
-    todo!()
-}
-
-
-#[proc_macro_derive(ToHashMap)]
-pub fn to_hashmap(input: TokenStream) -> TokenStream {
     todo!()
 }
