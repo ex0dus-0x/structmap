8663c28e99bd6337318cf9937b2811824a94fbf9
ex0dus-0x <ex0dus@codemuch.tech>
2020-12-27 15:30:12 -0500

    Refactor to support generic values

--- src/value.rs
+++ src/value.rs
@@ -2,15 +2,16 @@ use std::any::Any;
 use std::fmt;
 
 /// Represents primitive types that are supported for conversion into a HashMap that can support
-/// heterogeneous values. Inspired by `serde_json::Value`s. Exported and copied from derive macro
-/// crate to make publicly available for users.
+/// heterogeneous values. Inspired by `serde_json::Value`s.
 #[derive(Debug, Clone)]
 pub enum Value {
     Null,
     Bool(bool),
     Int(i32),
     UInt(u32),
-    String(&'static str),
+    String(String),
+    Array(Vec<Value>),
+    // TODO: Map
 }
 
 impl fmt::Display for Value {
@@ -20,6 +21,9 @@ impl fmt::Display for Value {
 }
 
 impl Value {
+
+    /// Given a genericized input type, encapsulate it as a Value that can be used in a map
+    /// container type when converting to and from a struct.
     pub fn to_value<T: Any>(value: T) -> Value {
         let any_val = &value as &dyn Any;
         if let Some(val) = any_val.downcast_ref::<bool>() {
@@ -29,13 +33,13 @@ impl Value {
         } else if let Some(val) = any_val.downcast_ref::<u32>() {
             Value::UInt(*val)
         } else if let Some(val) = any_val.downcast_ref::<&'static str>() {
-            Value::String(val)
+            Value::String(val.to_string())
+        } else if let Some(val) = any_val.downcast_ref::<String>() {
+            Value::String(val.to_string())
+        } else if let Some(val) = any_val.downcast_ref::<Vec<Value>>() {
+            Value::Array(val.to_vec())
         } else {
             Value::Null
         }
-    }
-
-    pub fn from_value<T: Any>(value: Value) -> T {
-        todo!()
     }
 }
