35f441c8c12c8dbf4e14bd9b5f722b3110c9eb9e
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2021-01-03 22:27:06 -0500

    Lint and cleanup README

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! Implements the public traits that developers inherit from in order to properly utilize the
 //! derive macro's functionality in code conversion and generation.
+
 pub mod value;
 
 use crate::value::Value;
@@ -9,6 +10,7 @@ pub trait FromHashMap: Default {
     fn from_hashmap(hashmap: HashMap<String, Value>) -> Self;
 }
 
+#[allow(clippy::wrong_self_convention)]
 pub trait ToHashMap: Default {
     fn to_hashmap(structure: Self) -> HashMap<String, Value>;
 }
@@ -17,6 +19,7 @@ pub trait FromBTreeMap: Default {
     fn from_btreemap(btreemap: BTreeMap<String, Value>) -> Self;
 }
 
+#[allow(clippy::wrong_self_convention)]
 pub trait ToBTreeMap: Default {
     fn to_btreemap(structure: Self) -> BTreeMap<String, Value>;
 }
